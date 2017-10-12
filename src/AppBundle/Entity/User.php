37831c42738980633738956ea591791555f79121
Bryan <bragudelo@comfamiliar.com>
2017-10-12 15:27:37 -0500

    Formularios, set en base de datos

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ * @UniqueEntity(fields="email", message="Este email ya esta en uso")
+ */
+class User  implements UserInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=false, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=64)
+     */
+    private $password;
+
+    /**
+   * @Assert\NotBlank()
+   * @Assert\Length(max=4096)
+   */
+  private $plainPassword;
+
+  /**
+       * @var string
+       *
+       * @ORM\Column(name="role", type="string", length=100, options={"default" = "ROLE_USER"})
+       *
+       */
+      private $role;
+
+      /**
+       * @var bool
+       *
+       * @ORM\Column(name="is_active", type="boolean", options={"default" = "1"})
+       */
+      private $isActive;
+
+#################################
+####### GETTERS Y SETTERS########
+#################################
+
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPlainPassword($password)
+    {
+        $this->plainPassword = $password;
+    }
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+         * Set role
+         *
+         * @param string $role
+         *
+         * @return User
+         */
+        public function setRole($role)
+        {
+            $this->role = $role;
+
+            return $this;
+        }
+
+        /**
+         * Get role
+         *
+         * @return string
+         */
+        public function getRole()
+        {
+            return $this->role;
+        }
+
+        /**
+         * Set isActive
+         *
+         * @param boolean $isActive
+         *
+         * @return User
+         */
+        public function setIsActive($isActive)
+        {
+            $this->isActive = $isActive;
+
+            return $this;
+        }
+
+        /**
+         * Get isActive
+         *
+         * @return bool
+         */
+        public function getIsActive()
+        {
+            return $this->isActive;
+        }
+
+    public function getSalt()
+    {
+       // The bcrypt algorithm doesn't require a separate salt.
+       // You *may* need a real salt if you choose a different encoder.
+       return null;
+    }
+
+   public function getRoles()
+   {
+     return array('ROLE_USER');
+   }
+
+   public function eraseCredentials()
+   {
+
+   }
+}
---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+      $builder
+      ->add('username', TextType::class)
+      ->add('email', EmailType::class)
+      ->add('plainPassword', RepeatedType::class, array(
+              'type' => PasswordType::class,
+              'first_options'  => array('label' => 'Contraseña'),
+              'second_options' => array('label' => 'Repite contraseña')
+          ))
+      //->add('role', HiddenType::class, array('by_reference' => true))
+      //->add('isactive', HiddenType::class, array('by_reference' => true))
+      ->add('guardar', SubmitType::class)
+      ->add('borrar', ResetType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+
+}
